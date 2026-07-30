5ea653d0f90a4252b6e284170c49e3bedd8f7fb5
qqqjah <qqqjah@users.noreply.github.com>
2026-07-30 10:54:34 +0800

    Add docs folder for GitHub Pages

---
+++ docs/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'dayue-workbench-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(fetchResponse => {
+        if (!fetchResponse || fetchResponse.status !== 200) return fetchResponse;
+        const responseClone = fetchResponse.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        return fetchResponse;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
